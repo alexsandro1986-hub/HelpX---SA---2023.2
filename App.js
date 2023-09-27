6d53cf6adffb8c3d34097e73f466d1ff08e94b3a
GISELE MARA DOS ANJOS <gisele_anjos@estudante.sesisenai.org.br>
2023-09-27 17:57:07 -0300

    Tela de tratamento header alterado

--- App.js
+++ App.js
@@ -36,7 +36,7 @@ function MyStack() {
           },
           headerTintColor: '#fff',
           headerTitleStyle: {
-          fontWeight: 'bold',
+          fontWeight: '700',
           },
           headerTitleAlign: "center",
         }}/>
