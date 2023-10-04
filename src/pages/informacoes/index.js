ec0dc3e619b6aaf69eb1ef413d9125d91f1a040d
Alexsandro silva <alexsandrosilvasilva886@gmail.com>
2023-10-04 17:50:08 -0300

    Add files via upload

--- src/pages/informacoes/index.js
+++ src/pages/informacoes/index.js
@@ -1,151 +1,375 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Button, Input } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { View, Button, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function informacoes(navigation) {
-  
-    const avancar = () => {
-      alert('oi')
-    }
 
-    const voltar = () => {
-      alert('calma gisele')
-    }
-    
+const Stack = createStackNavigator();
+const Header = () => (
+
+    <View style={styles.header}>
+        <View style={styles.containerinfromacoes} >
+            <Text style={styles.textoInforma}>INFORMAÇÕES</Text>
+        </View>
+    </View>
+
+);
+
+export default function informacoes() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
-        <View style={styles.informacoes}>
-          <View>
-            <Text style={styles.informatexto}>INFORMAÇOES!</Text>
-          </View>
+        <><Header />
 
-          <View style={styles.caixaIforma}>
-            <Text style={styles.textoInforma}>Pedido Informações</Text>
-            <View>
-              <Input
-                placeholder='Informações...'
-                keyboardType='email-address'
+            <Stack.Navigator >
 
-                onChangeText={value => setEmail(value)}
-                returnKeyType='done'
-              />
+                <Stack.Group >
 
-            </View>
+                    <Stack.Screen name='Nome' component={Nome('Idade')} options={{ headerShown: false }} />
+                    <Stack.Screen name='Idade' component={Idade('Alergias')} options={{ headerShown: false }} />
+                    <Stack.Screen name='Alergias' component={Alergias('Contatos')} options={{ headerShown: false }} />
+                    <Stack.Screen name='Contatos' component={Contatos('Endereco')} options={{ headerShown: false }} />
+                    <Stack.Screen name='Endereco' component={Endereco('')} options={{ headerShown: false }} />
 
-          </View>
+                </Stack.Group>
 
+            </Stack.Navigator></>
 
-
-
-
-        </View>
-        <View style={styles.botaoDireito}>
-
-          <Button
-            icon={
-              <Icon
-                name="arrow-right"
-                size={15}
-                color="white"
-
-              />
-            }
-
-            onPress={avancar}
-          />
-
-        </View>
-        <View style={styles.botaoEsquerdo}>
-          <Button
-            icon={
-              <Icon
-                name="arrow-left"
-                size={15}
-                color="white"
-
-              />
-            }
-
-            onPress={voltar}
-          />
-
-        </View>
-      </View>
     );
-  }
-  const Tab = createBottomTabNavigator();
+}
+
+function Nome(a) {
+    return function ({ navigation }) {
+
+        return (
+            <View style={styles.caixa}>
+                <View style={styles.body}>
+                    <View style={styles.teste}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Nome"
+                        // onChangeText={(text) => setNome(text)}
+                        // value={nome}
+                        />
+                    </View>
+                    <View style={styles.botao}>
+                        <TouchableOpacity onPress={() => navigation.navigate(a)}>
+
+                            <Text style={styles.bto_Direita}>
+
+                                <Icon
+                                    name="arrow-right"
+                                    size={35}
+                                    color="white"
+
+                                />
+
+                            </Text>
 
 
 
-  // return (
-  //   <Tab.Screen
-  //     name="informacoes"
-  //     component={informacoes}
-  //     options={{
-  //       tabBarLabel: 'informacoes',
-  //       tabBarIcon: ({ color, size }) => (
-  //         <MaterialCommunityIcons name="account" color={color} size={size} />
-  //       ),
-  //     }}
-  //   />
+                        </TouchableOpacity >
+                        <TouchableOpacity onPress={() => navigation.navigate()} >
+                            <Text style={styles.esquerda_bto}>
+                                <Icon
+                                    name="arrow-left"
+                                    size={35}
+                                    color="white"
 
-  // );
+                                />
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+function Idade(a) {
+    return function ({ navigation }) {
+        return (
+            <View style={styles.caixa}>
+                <View style={styles.body}>
+                    <View style={styles.teste}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Idade"
+                        // onChangeText={(text) => setNome(text)}
+                        // value={nome}
+                        />
+                    </View>
+                    <View style={styles.botao}>
+                        <TouchableOpacity onPress={() => navigation.navigate(a)}>
+
+                            <Text style={styles.bto_Direita}>
+                                <Icon
+                                    name="arrow-right"
+                                    size={35}
+                                    color="white"
+
+                                />
+                            </Text>
 
 
+
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={() => navigation.navigate()} >
+                            <Text style={styles.esquerda_bto}>
+                                <Icon
+                                    name="arrow-left"
+                                    size={35}
+                                    color="white"
+
+                                />
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+function Alergias(a) {
+    return function ({ navigation }) {
+        return (
+            <View style={styles.caixa}>
+                <View style={styles.body}>
+                    <View style={styles.teste}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Alergias"
+                        // onChangeText={(text) => setNome(text)}
+                        // value={nome}
+                        />
+                    </View>
+                    <View style={styles.botao}>
+                        <TouchableOpacity onPress={() => navigation.navigate(a)}>
+
+                            <Text style={styles.bto_Direita}>
+                                <Icon
+                                    name="arrow-right"
+                                    size={35}
+                                    color="white"
+
+                                />
+                            </Text>
+                            <Text style={styles.esquerda_bto}>
+                                <Icon
+                                    name="arrow-left"
+                                    size={35}
+                                    color="white"
+
+                                />
+                            </Text>
+
+
+                        </TouchableOpacity>
+
+
+                    </View>
+                </View></View>
+        );
+    }
+}
+
+
+function Contatos(a) {
+    return function ({ navigation }) {
+        return (
+            <View style={styles.caixa}>
+                <View style={styles.body}>
+                    <View style={styles.teste}>
+                        <TextInput
+                            style={styles.input_Contato}
+                            placeholder="N°-telefone"
+
+                        />
+                        <TextInput
+                            style={styles.input_Contato}
+                            placeholder="Nome-contato"
+
+                        />
+                        <TextInput
+                            style={styles.input_Contato}
+                            placeholder="Telefone-contato"
+
+                        />
+                    </View>
+
+                    <View style={styles.botao}>
+                        <TouchableOpacity onPress={() => navigation.navigate(a)}>
+
+                            <Text style={styles.bto_Direita}>
+                                <Icon
+                                    name="arrow-right"
+                                    size={35}
+                                    color="white"
+
+                                />
+                            </Text>
+                            <Text style={styles.esquerda_bto}>
+                                <Icon
+                                    name="arrow-left"
+                                    size={35}
+                                    color="white"
+
+                                />
+                            </Text>
+
+
+                        </TouchableOpacity></View>
+                </View></View>
+        );
+    }
+}
+
+
+function Endereco(a) {
+    return function ({ navigation }) {
+        return (
+            <View style={styles.caixa}>
+                <View style={styles.body}>
+                    <View style={styles.teste}>
+                        <TextInput
+                            style={styles.input_endereco}
+                            placeholder="N°-CEP"
+
+                        />
+                        <TextInput
+                            style={styles.input_endereco}
+                            placeholder="Logradouro"
+
+                        />
+                        <TextInput
+                            style={styles.input_endereco}
+                            placeholder="Numero"
+
+                        />
+                    </View>
+                    <View style={styles.botao}>
+                        <TouchableOpacity onPress={() => navigation.navigate(a)}>
+
+                            <Text style={styles.bto_Direita}>
+                                <Icon
+                                    name="arrow-right"
+                                    size={35}
+                                    color="white"
+
+                                />
+                            </Text>
+                            <Text style={styles.esquerda_bto}>
+                                <Icon
+                                    name="arrow-left"
+                                    size={35}
+                                    color="white"
+
+                                />
+                            </Text>
+
+
+                        </TouchableOpacity></View>
+                </View></View>
+        );
+    }
+}
+
+
+
+const Tab = createBottomTabNavigator();
 
 
 const styles = StyleSheet.create({
-  informacoes: {
-    flex: 1,
-    width: 450,
-    backgroundColor: "#C7FFCC",
-    alignItems: "center",
-    gap: 42
-
-  },
-  informatexto: {
-    marginTop: 100,
-    fontSize: 30,
-    color: '#e91e63',
-  },
-  caixaIforma: {
-    height: 350,
-    width: 350,
-    backgroundColor: '#FFF',
-    borderRadius: 15,
-    alignItems: 'center'
-  },
-  textoInforma: {
-    fontSize: 30,
-    marginTop: 10,
-  },
-  pedirInfo: {
-    marginTop: 45,
-    fontSize: 50,
-  },
-  botaoDireito: {
-    display: 'flex',
-    left: 105,
-    top: 20
+    header: {
+        height: 250,
+        backgroundColor: "#C7FFCC",
 
 
-  },
-  botaoEsquerdo: {
-    right: 105,
-    bottom: 10
-  },
+    },
+    containerinfromacoes: {
+        flex: 1,
+        alignItems: "center",
+
+    },
+
+    textoInforma: {
+        marginTop: 180,
+        fontSize: 40,
+        color: '#fff',
+    },
 
 
 
+    caixa: {
+        backgroundColor: "#C7FFCC",
+    },
+    body: {
+
+        backgroundColor: '#FF007ACC',
+        width: 378,
+        height: 500,
+        alignItems: 'center',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        borderRadius: 15
+    },
+    input: {
+        fontSize: 30,
+        marginBottom: 250,
+        gap: 10,
+        padding: 10,
+        borderBottomWidth: 1,
+    },
+    input_Contato: {
+        fontSize: 30,
+        marginBottom: 30,
+        borderBottomWidth: 1,
+
+    },
+    input_endereco: {
+        fontSize: 30,
+        marginBottom: 30,
+        borderBottomWidth: 1,
+    },
+    botao: {
+        gap: 25,
+        padding: 45,
+    },
+    bto_Direita: {
+        display: 'flex',
+        left: 155,
+        bottom: 60,
 
 
 
+    },
+    esquerda_bto: {
+        right: 155,
+        bottom: 95
+    },
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
