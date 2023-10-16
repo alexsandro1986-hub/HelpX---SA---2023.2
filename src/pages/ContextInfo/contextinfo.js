fd646b79f5ef208f38e84ee333ac606b1ecf8ca1
ALEXSANDRO SILVA SILVA <alexsandro_silva16@estudante.sesisenai.org.br>
2023-10-16 16:16:16 -0300

    alteraçoes de Informacoes, cadatatro, e context

--- src/pages/ContextInfo/contextinfo.js
+++ src/pages/ContextInfo/contextinfo.js
@@ -3,20 +3,44 @@ import React from 'react';
 
 export const ContextInfo = React.createContext()
 
-export const ContextInfoProvider = ({children}) => {
+export const ContextInfoProvider = ({ children }) => {
+    //Erick
+    const [inputEmail, setInputEmail] = useState('')
+    const [inputSenha, setInputSenha] = useState('')
+    const [inputConfirmaSenha, setInputConfirmaSenha] = useState('')
+    //Alex
     const [inputNome, setInputNome] = useState('')
     const [inputIdade, setInputIdade] = useState('')
+    const [inputAlergias, setInputAlergias] = useState('')
     const [inputTelefone, setInputTelefone] = useState('')
-    const [inputEmail, setInputEmail] = useState ('')
+    const [inputContatoEmergencia, setInputContatoEmergencia] = useState('')
+    const [inputNtelefoneEmergencia, setNtelefoneEmergencia] = useState('')
+    const [inputNCep,setInputNcep]= useState('')
+    const [inputLogradouro,setInputLogradouro]= useState('')
+    const [inputNumeroCasa,setNumeroCasa]= useState('')
 
-  
+
     return (
         <ContextInfo.Provider
-            value={{inputNome, setInputNome,
+            value={{
+               //Ercik
+                inputEmail, setInputEmail,
+                inputSenha, setInputSenha,
+                inputConfirmaSenha, setInputConfirmaSenha,
+                //Alexs
+                inputNome, setInputNome,
                 inputIdade, setInputIdade,
                 inputTelefone, setInputTelefone,
-                inputEmail, setInputEmail
-                  }}>
+                inputAlergias, setInputAlergias,
+                inputTelefone, setInputTelefone,
+                inputContatoEmergencia, setInputContatoEmergencia,
+                inputNtelefoneEmergencia, setNtelefoneEmergencia,
+                inputNCep,setInputNcep,
+                inputLogradouro,setInputLogradouro,
+                inputNumeroCasa,setNumeroCasa
+
+                
+            }}>
             {children}
         </ContextInfo.Provider>
 
